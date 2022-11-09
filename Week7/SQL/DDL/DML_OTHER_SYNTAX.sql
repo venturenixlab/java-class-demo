7aa67127af7647d9de0b2df17edb8816979c87ba
vincent.lau <vincent.lau@venturenix.com>
2022-11-09 13:18:04 +0800

    update

---
+++ Week7/SQL/DDL/DML_OTHER_SYNTAX.sql
@@ -0,0 +1,13 @@
+-- Create a new table by cloning table struture from another table
+-- No data will be cloned
+CREATE TABLE worker_clone LIKE worker;
+
+-- Clone data, if the table structure are same
+ -- You can add where clause to filter targeted data set to be cloned.
+INSERT INTO worker_clone
+SELECT * FROM worker;
+
+-- return current date
+SELECT CURDATE();
+-- return current date and time
+SELECT NOW();
---
+++ Week7/SQL/DDL/EXERCISE2.sql
@@ -0,0 +1,68 @@
+-- create new database
+CREATE DATABASE ORG;
+-- show all database;
+SHOW DATABASES;
+-- get into database
+USE ORG;
+
+-- create table worker
+CREATE TABLE WORKER (
+	WORKER_ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY NUMERIC(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+-- inesrt data to worker
+INSERT INTO WORKER 
+	(FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		('Monika', 'Arora', 100000, '21-02-20 09:00:00', 'HR'),
+		('Niharika', 'Verma', 80000, '21-06-11 09:00:00', 'Admin'),
+		('Vishal', 'Singhal', 300000, '21-02-20 09:00:00', 'HR'),
+		('Amitabh', 'Singh', 500000, '21-02-20 09:00:00', 'Admin'),
+		('Vivek', 'Bhati', 490000, '21-06-11 09:00:00', 'Admin'),
+		('Vipul', 'Diwan', 200000, '21-06-11 09:00:00', 'Account'),
+		('Satish', 'Kumar', 75000, '21-01-20 09:00:00', 'Account'),
+		('Geetika', 'Chauhan', 90000, '21-04-11 09:00:00', 'Admin');
+
+-- create table bonus
+CREATE TABLE BONUS (
+	WORKER_REF_ID INTEGER,
+	BONUS_AMOUNT NUMERIC(10),
+	BONUS_DATE DATETIME,
+	FOREIGN KEY (WORKER_REF_ID)
+		REFERENCES Worker(WORKER_ID)
+);
+
+-- Task 1:
+-- Vivek, with amount 32000 and bonus date 2021 Nov 02
+-- Vivek, with amount 20000 and bonus date 2022 Nov 02
+-- Amitabh, with amount 21000 and bonus date 2021 Nov 02
+-- Geetika, with amount 30000 and bonus date 2021 Nov 02
+-- Satish, with amount 4500 and bonus date 2022 Nov 02
+
+-- Task 2:
+-- Write an SQL query to show the second highest salary among all workers.
+
+-- Task 3:
+-- Write an SQL query to print the name of employees having the highest salary in each department.
+
+-- Task 4:
+-- Write an SQL query to fetch the list of employees with the same salary.
+
+-- Task 5:
+-- Write an SQL query to find the worker names with salaries + bonus in 2021
+
+-- Task 6 (Please complete Task 1-5 first):
+-- Try to delete all the records in table WORKER
+-- Study the reason why the data cannot be deleted
+-- Try to delete all the records in table BONUS
+-- Try to delete all the records in table WORKER
+
+-- Task 7 (Please complete Task 6 first):
+-- Try to drop table WORKER
+-- Study the reason why the table cannot be deleted
+-- Try to drop table BONUS
+-- Try to drop table WORKER
