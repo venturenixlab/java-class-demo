bc01217b507d574baa4d63e0875beaa1a2ac5aaa
vincent.lau <vincent.lau@venturenix.com>
2022-11-09 13:29:38 +0800

    update

---
+++ Week7/SQL/DDL/DML_FUNCTION.sql
@@ -0,0 +1,19 @@
+-- Upper case the string
+Select upper(FIRST_NAME) from Worker;
+-- Lower case the string
+Select lower(FIRST_NAME) from Worker;
+
+-- Yes, SQL substring like Java string.substrin()
+Select substring(FIRST_NAME,1,3) from Worker;
+
+-- Remove the space from the right hand side of the string
+Select RTRIM(FIRST_NAME) from Worker;
+-- Remove the space from the left hand side of the string
+Select RTRIM(FIRST_NAME) from Worker;
+-- Remove the space from the string
+Select TRIM(FIRST_NAME) from Worker;
+
+-- Replace
+Select REPLACE(FIRST_NAME,'a','A') from Worker;
+-- Concat
+Select CONCAT(FIRST_NAME,'a','A') from Worker;
