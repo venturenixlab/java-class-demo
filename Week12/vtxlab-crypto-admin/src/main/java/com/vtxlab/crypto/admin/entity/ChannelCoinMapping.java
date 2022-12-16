ab19ec8e6f32b5be56196966d16b2788d8b56095
vincent.lau <vincent.lau@venturenix.com>
2022-12-16 20:57:35 +0800

    update

--- Week12/vtxlab-crypto-admin/src/main/java/com/vtxlab/crypto/admin/entity/ChannelCoinMapping.java
+++ Week12/vtxlab-crypto-admin/src/main/java/com/vtxlab/crypto/admin/entity/ChannelCoinMapping.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
